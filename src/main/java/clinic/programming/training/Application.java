4076c81a2ee476f80498aafcd5b4bb60206c2d9f
Rajeev Asthana <rajeev.asthana@gmail.com>
2020-08-23 02:59:07 +0530

    Use string utils and adding dependencies to lang3 and Adding unit testing

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -2,8 +2,15 @@ package clinic.programming.training;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
+
+   public int countWords(String words){
+
+   String[] separateWords = StringUtils.split(words, ' ');
+   return (separateWords == null) ? 0 : separateWords.length;
+   } 
 
    public void greet () {
 
@@ -22,6 +29,8 @@ public class Application {
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 	Application app = new Application();
-        app.greet(); 
+        app.greet();
+        int count = app.countWords("I have four words");
+        System.out.println("Word Count: " + count);
     }
 }
